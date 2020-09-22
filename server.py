bb28ba2a7ebf876cbc90f801763f058905cab9e9
Elijah Luckey <luckeyelijah112@gmail.com>
2020-09-22 11:26:12 -0400

    Converted some object to final if reused and not changed

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from typing import Final
 
 update_id = None
 RESULT_KEY: Final = "result"
+MESSAGE_KEY: Final = "message"
 CONFIG_FILE = "token.cfg"
 
 
@@ -23,14 +24,14 @@ while True:
             update_id = item["update_id"]
             try:
                 # fetching message content form Json
-                message = item["message"]["text"]
+                message = item[MESSAGE_KEY]["text"]
             except ValueError():
                 message = None
             # gets the id of the person who sent the message
             # to the bot
-            from_ = item["message"]["from"]["id"]
+            from_ = item[MESSAGE_KEY]["from"]["id"]
 
             reply = make_reply(message)
-            print('REPLYING TO:', item["message"]["from"]["username"])
+            print('REPLYING TO:', item[MESSAGE_KEY]["from"]["username"])
             print('REPLYING:', reply)
             b.send_message(reply, from_)
